fe0438e152596019c96ea2e0b377461fbc03cbf0
Asmit Raj <asmitraj9472@gmail.com>
2026-04-14 22:23:56 +0530

    Add dotenv configuration to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,8 @@ import jwt from "jsonwebtoken"
 import cookieParser from "cookie-parser"
 import User from "./models/userModel.js"
 import { fileURLToPath } from "url"
-
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 
