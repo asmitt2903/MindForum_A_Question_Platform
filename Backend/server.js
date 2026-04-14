42dc6c5706250a38267ed1b70729f7942849a42e
Asmit Raj <asmitraj9472@gmail.com>
2026-04-14 22:13:17 +0530

    Refactor server to use environment variable for port

--- Backend/server.js
+++ Backend/server.js
@@ -126,7 +126,7 @@ app.get("/logout", (req, res) => {
     res.redirect("/login");
 });
 
-
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
     console.log("Server running on port 3000")
 })
